d8a9f2b6a3f124219e49f86127b86f2af9b923da
Sunil Thakur <sunilthakur178co@gmail.com>
2024-08-12 15:03:07 +0530

    Script for building curl with openssl and zlib for android

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ ! -d "$ANDROID_NDK_HOME" ]; then
+    echo "Please check ANDROID_NDK_HOME directory"
+    exit 1
+fi
+
+if [ -z "$ANDROID_NDK_HOME" ]; then
+    echo "Please set your ANDROID_NDK_HOME environment variable first"
+    exit 1
+fi
+
+if [[ "$ANDROID_NDK_HOME" == .* ]]; then
+    echo "Please set your ANDROID_NDK_HOME to an absolute path"
+    exit 1
+fi
+
+export TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_TAG
+
+export CFLAGS="-Os"
+export LDFLAGS="-Wl,-Bsymbolic"
+
+chmod +x ./build-zlib.sh
+chmod +x ./build-openssl.sh
+chmod +x ./build-curl.sh
+
+./build-zlib.sh
+./build-openssl.sh
+./build-curl.sh
+
+exit 0
